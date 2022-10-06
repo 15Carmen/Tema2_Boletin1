0c55c25a46bf416234502658d180f40d47d3fafe
cmartin <carmen.martin@iesnervion.es>
2022-10-06 14:41:06 +0200

    commit4

--- src/ejercicios/Ejercicio6.java
+++ src/ejercicios/Ejercicio6.java
@@ -1,5 +1,7 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio6 {
     /*
      * Pedir los coeficientes de una ecuación de segundo grado y mostrar sus soluciones reales. Si no existen,
@@ -7,7 +9,37 @@ public class Ejercicio6 {
      * (ax2 + bx + c = 0) se calculan x=(-b+-raizCuadrada(b^2-(4ac)))/2a
      */
     public static void main(String[] args) {
-        int a, b, c;
+       //Declaramos las variables
+        int a, b, c; //Coeficientes que el usuario va a introducir por consola
+        double x1, x2;
+
+        //Declaramos el scanner para poder leer por consola
+        Scanner sc = new Scanner(System.in);
+
+        //Le pedimos al usuario que introduzca los coeficientes y los guardamos en las variables homónimas
+        System.out.printf("Introduzca el coeficiente a: ");
+        a=sc.nextInt();
+        System.out.printf("Introduzca el coeficiente b: ");
+        b=sc.nextInt();
+        System.out.printf("Introduzca el coeficiente c: ");
+        c=sc.nextInt();
+
+        //calculamos la ecuación de segundo grado
+        if (a==0){
+            x1=(-c)/b;
+            System.out.println("El resultado de la ecuación "+a+"x2  "+b+"x + "+c+" = 0 es x="+x1);
+        } else if (b==0) {
+            System.out.println("El resultado de la ecuación "+a+"x2  "+b+"x + "+c+" = 0 no es un número real, es decir x = +- la raiz cuadrada de "+(-c)+"/"+a);
+        } else {
+            x1=(((-b)+Math.sqrt((Math.pow(b,2))-(4*a*c)))/(2*a));
+            x2=(((-b)-Math.sqrt((Math.pow(b,2))-(4*a*c)))/(2*a));
+
+            System.out.println("El resultado de la ecuación "+a+"x2  "+b+"x + "+c+" = 0 es x="+x1+" y x="+x2);
+        }
+
+
+
+
 
     }
 }
