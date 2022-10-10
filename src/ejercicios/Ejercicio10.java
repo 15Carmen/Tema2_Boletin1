875d41f167c7b60fad6ab8708e6fba67c866d4ed
cmartin <carmen.martin@iesnervion.es>
2022-10-10 10:12:36 +0200

    commit5

--- src/ejercicios/Ejercicio10.java
+++ src/ejercicios/Ejercicio10.java
@@ -1,11 +1,39 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio10 {
     /*
      * Escribir un programa que pida al usuario tres números enteros, y que muestre por pantalla si la
      * suma de dos de esos números da como resultado el otro número.
      */
     public static void main(String[] args) {
+
+        //Declaramos las variables
+        int num1, num2, num3; //Variables enteras donde vamos a guardar los números introducidos por el usuario
+        int resultado; //Resultado de la suma de dos de los números
+
+        //Declaramos el scanner para poder leer por consola
+        Scanner sc = new Scanner (System.in);
+
+        //Le pedimos al usuario que introduzca 3 numeros enteros y los guardamos en las variables declaradas previamente
+        System.out.println("Introduzca un numero entero:");
+        num1= sc.nextInt();
+        System.out.println("Introduzca otro numero entero:");
+        num2= sc.nextInt();
+        System.out.println("Introduzca otro mas:");
+        num3= sc.nextInt();
+
+        if (num1+num2==num3){
+            System.out.println("La suma de "+num1+" y "+num2+" es "+num3);
+        }else if (num1+num3==num2){
+            System.out.println("La suma de "+num1+" y "+num3+" es "+num2);
+        }else if (num2+num3==num1){
+            System.out.println("La suma de "+num2+" y "+num3+" es "+num1);
+        }else {
+            System.out.println("Ninguno de los numeros sumados entre si da otro de los numeros introducidos");
+        }
+
 
     }
 }
--- src/ejercicios/Ejercicio9.java
+++ src/ejercicios/Ejercicio9.java
@@ -1,5 +1,7 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio9 {
     /*
      * Escribir un programa que simule el juego de PIEDRA, PAPEL, TIJERA, pidiendo a cada jugador
@@ -8,5 +10,39 @@ public class Ejercicio9 {
      */
     public static void main(String[] args) {
 
+        //Declaramos las constantes
+        final int PIEDRA = 1;
+        final int PAPEL = 2;
+        final int TIJERAS = 3;
+
+        //Declaramos las variables donde vamos a guardar las respuestas del usuario introducidas por consola
+        int jugador1, jugador2;
+
+        //Declaramos el scanner para poder leer por consola
+        Scanner sc = new Scanner(System.in);
+
+        //Le pedimos al jugador 1 que introduzca una de las opciones posibles y la guardamos en la variable jugador1
+        System.out.println("Jugador 1, elija piedra[1], papel[2] o tijeras[3]: ");
+        jugador1 = sc.nextInt();
+
+        //Le pedimos al jugador 2 que introduzca una de las opciones posibles y la guardamos en la variable jugador2
+        System.out.println("Jugador 2, elija piedra[1], papel[2] o tijeras[3]: ");
+        jugador2 = sc.nextInt();
+
+        //Si alguno de los numeros introducidos no se encuentra en el rango (1,3) enviamos un mensaje de error
+        if (jugador1 < 1 || jugador1 > 3 && jugador2 < 1 || jugador2 > 3) {
+            System.out.println("Numero introducido no valido");
+        } else {
+            if (jugador1 == jugador2) { //Si los numeros introducidos son iguales entonces hay un empate
+                System.out.println("EMPATE");
+            } else if (jugador1 == PIEDRA && jugador2 == TIJERAS || // Si jugador1=piedra y jugador2=tijeras o
+                    jugador1 == PAPEL && jugador2 == PIEDRA || // Si jugador1=papel y jugador2=piedra o
+                    jugador1 == TIJERAS && jugador2 == PAPEL) { // Si jugador1=tijeras y jugador2=papel
+                System.out.println("Gana el jugador 1");
+            } else { //Si no
+                System.out.println("Gana el jugador 2");
+            }
+        }
     }
 }
+
